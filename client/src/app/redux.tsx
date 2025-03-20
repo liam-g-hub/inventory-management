376a003301e67a4f1a3a789d723890f293400dde
liam-g-hub <137252188+liam-g-hub@users.noreply.github.com>
2025-03-20 13:17:24 +0800

    Update redux.tsx

--- client/src/app/redux.tsx
+++ client/src/app/redux.tsx
@@ -26,13 +26,13 @@ import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 /* REDUX PERSISTENCE */
 const createNoopStorage = () => {
   return {
-    getItem(_key: any) {
+    getItem() {
       return Promise.resolve(null);
     },
-    setItem(_key: any, value: any) {
-      return Promise.resolve(value);
+    setItem() {
+      return Promise.resolve(null);
     },
-    removeItem(_key: any) {
+    removeItem() {
       return Promise.resolve();
     },
   };
