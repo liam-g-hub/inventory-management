507d16d7fdcba1d849edcd9c8efd5d1bd065d349
liam-g-hub <137252188+liam-g-hub@users.noreply.github.com>
2025-03-20 12:49:27 +0800

    Update next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,21 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
-eslint: {
-    ignoreDuringBuilds: true
-}
-
-module.exports = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-}
-
-export default nextConfig;
